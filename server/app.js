e446b720fa20d2cc12b6914d0ed416fcc2c491ce
Robert <www.robertpat92@gmail.com>
2018-12-21 15:00:58 +0100

    ala

--- server/app.js
+++ server/app.js
@@ -3,11 +3,11 @@ const bodyparser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 
-
-const api = require('./routes/api');
-const PORT = (process.env.PORT || 8080);
-
 const app = express();
+const api = require('./routes/api');
+const PORT = app.listen(process.env.PORT || 8080);
+
+
 
 app.use(cors());
 
