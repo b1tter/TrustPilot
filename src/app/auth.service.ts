51a922d1227786d197e015380cefd9edfc40d78b
Robert <www.robertpat92@gmail.com>
2018-12-24 13:16:09 +0100

    New try6

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -7,8 +7,8 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-  private _registerUrl = 'api/register';
-  private _loginUrl = 'api/login';
+  private _registerUrl = 'http://localhost:8080/api/register';
+  private _loginUrl = 'http://localhost:8080/api/login';
   constructor(private http: HttpClient,
               private _router: Router) { }
 
